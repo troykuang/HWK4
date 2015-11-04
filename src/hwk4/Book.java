235a09e9f6b16e1247bb8277ed4086dddbcc4689
Troy Kuang <troykuang0608@gmail.com>
2015-11-03 22:02:31 -0500

    initial commit

---
+++ src/hwk4/Book.java
@@ -0,0 +1,13 @@
+package hwk4;
+
+public class Book extends Readable{
+	public int getPrice(){
+		return (int (price*1.02))
+	}
+	
+	public int  intgetListInfo(){
+		//Based on the value of Type(Book
+		//or eBook) print the list of Items
+	}
+
+}
