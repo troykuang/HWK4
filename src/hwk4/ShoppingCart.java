c0b81a68c0270032e74c137ff82ee3a525c3710a
Troy Kuang <troykuang0608@gmail.com>
2015-11-19 14:26:50 -0500

    file process

--- src/hwk4/ShoppingCart.java
+++ src/hwk4/ShoppingCart.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class ShoppingCart extends User{
 	private ArrayList content ;//array of items
 	public void makeCart(){
+		String path = "Cart_["+this.username+"]" ;
+		
 	//TODO IS SHOOPING CART FILE EXIST / NOT EXIST 
 	}
 
